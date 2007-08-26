bymerej/mwphb

commit c97e7509e6070146d94da262c7e9c516fae3e42f

(bug 10683) Fix inconsistent handling of URL-encoded titles in links used in redirects (i.e. they now work)
svn path=/trunk/phase3/; revision=24505

includes/Title.php
@@ -269,32 +269,27 @@ class Title {
 	}
 
 	/**
-	 * Create a new Title for a redirect
-	 * @param string $text the redirect title text
-	 * @return Title the new object, or NULL if the text is not a
-	 *	valid redirect
+	 * Extract a redirect destination from a string and return the
+	 * Title, or null if the text doesn't contain a valid redirect
+	 *
+	 * @param string $text Text with possible redirect
+	 * @return Title
 	 */
 	public static function newFromRedirect( $text ) {
-		$mwRedir = MagicWord::get( 'redirect' );
-		$rt = NULL;
-		if ( $mwRedir->matchStart( $text ) ) {
-			$m = array();
-			if ( preg_match( '/\[{2}(.*?)(?:\||\]{2})/', $text, $m ) ) {
-				# categories are escaped using : for example one can enter:
-				# #REDIRECT [[:Category:Music]]. Need to remove it.
-				if ( substr($m[1],0,1) == ':') {
-					# We don't want to keep the ':'
-					$m[1] = substr( $m[1], 1 );
-				}
-
-				$rt = Title::newFromText( $m[1] );
-				# Disallow redirects to Special:Userlogout
-				if ( !is_null($rt) && $rt->isSpecial( 'Userlogout' ) ) {
-					$rt = NULL;
-				}
+		$redir = MagicWord::get( 'redirect' );
+		if( $redir->matchStart( $text ) ) {
+			// Extract the first link and see if it's usable
+			if( preg_match( '!\[{2}(.*?)(?:\||\]{2})!', $text, $m ) ) {
+				// Strip preceding colon used to "escape" categories, etc.
+				// and URL-decode links
+				$m[1] = urldecode( ltrim( $m[1], ':' ) );
+				$title = Title::newFromText( $m[1] );
+				// Redirects to Special:Userlogout are not permitted
+				if( $title instanceof Title && !$title->isSpecial( 'Userlogout' ) )
+					return $title;
 			}
 		}
-		return $rt;
+		return null;
 	}
 
 #----------------------------------------------------------------------------

RELEASE-NOTES
@@ -338,6 +338,8 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
   used on a non-existent page with "action=info"
 * Fix escaping of raw message text when used on a non-existent page with
   "action=info"
+* (bug 10683) Fix inconsistent handling of URL-encoded titles in links
+  used in redirects (i.e. they now work)
 
 == API changes since 1.10 ==
 

commit 149810745102a6334c36c3405cad38b925d363b7

Fix regression caused by r24505 for bug 10683 -- was breaking redirects to pages using '+' in the title. Now matching parser inline link decoding; urldecode() is only run if %s are present. This does break something that has a literal % *and* a literal +, but that already matches Parser behavior. :P Should think about tweaking that to maybe use rawurldecode?
svn path=/trunk/phase3/; revision=25161

includes/Title.php
@@ -283,7 +283,12 @@ class Title {
 			if( preg_match( '!\[{2}(.*?)(?:\||\]{2})!', $text, $m ) ) {
 				// Strip preceding colon used to "escape" categories, etc.
 				// and URL-decode links
-				$m[1] = urldecode( ltrim( $m[1], ':' ) );
+				if( strpos( $m[1], '%' ) !== false ) {
+					// Match behavior of inline link parsing here;
+					// don't interpret + as " " most of the time!
+					// It might be safe to just use rawurldecode instead, though.
+					$m[1] = urldecode( ltrim( $m[1], ':' ) );
+				}
 				$title = Title::newFromText( $m[1] );
 				// Redirects to Special:Userlogout are not permitted
 				if( $title instanceof Title && !$title->isSpecial( 'Userlogout' ) )