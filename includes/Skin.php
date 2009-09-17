bymerej/mwphb

commit e063844db2a9d8e7eac42795cdcee5e6c7efb51b

Fix live preview copying of existing category links
svn path=/trunk/phase3/; revision=55573

includes/Skin.php
@@ -902,14 +902,11 @@ END;
 
 		$classes = 'catlinks';
 
-		if( strpos( $catlinks, '<div id="mw-normal-catlinks">' ) === false &&
-			strpos( $catlinks, '<div id="mw-hidden-catlinks" class="mw-hidden-cats-hidden">' ) !== false ) {
+		if( strpos( $catlinks, '<div id="mw-normal-catlinks">' ) === false ) {
 			$classes .= ' catlinks-allhidden';
 		}
 
-		if( !empty( $catlinks ) ){
-			return "<div id='catlinks' class='$classes'>{$catlinks}</div>";
-		}
+		return "<div id='catlinks' class='$classes'>{$catlinks}</div>";
 	}
 
 	function getQuickbarCompensator( $rows = 1 ) {

includes/Article.php
@@ -2604,6 +2604,8 @@ class Article {
 		$wgOut->setSubtitle( wfMsgHtml( 'delete-backlink', $deleteBackLink ) );
 		$wgOut->setRobotPolicy( 'noindex,nofollow' );
 		$wgOut->addWikiMsg( 'confirmdeletetext' );
+		
+		wfRunHooks( 'ArticleConfirmDelete', array( $this, $wgOut, &$reason ) );
 
 		if( $wgUser->isAllowed( 'suppressrevision' ) ) {
 			$suppress = "<tr id=\"wpDeleteSuppressRow\" name=\"wpDeleteSuppressRow\">
@@ -2703,14 +2705,14 @@ class Article {
 				$wgOut->addWikiMsg( 'deletedtext', $deleted, $loglink );
 				$wgOut->returnToMain( false );
 				wfRunHooks('ArticleDeleteComplete', array(&$this, &$wgUser, $reason, $id));
+			}
+		} else {
+			if( $error == '' ) {
+				$wgOut->showFatalError( wfMsgExt( 'cannotdelete', array( 'parse' ) ) );
+				$wgOut->addHTML( Xml::element( 'h2', null, LogPage::logName( 'delete' ) ) );
+				LogEventsList::showLogExtract( $wgOut, 'delete', $this->mTitle->getPrefixedText() );
 			} else {
-				if( $error == '' ) {
-					$wgOut->showFatalError( wfMsgExt( 'cannotdelete', array( 'parse' ) ) );
-					$wgOut->addHTML( Xml::element( 'h2', null, LogPage::logName( 'delete' ) ) );
-					LogEventsList::showLogExtract( $wgOut, 'delete', $this->mTitle->getPrefixedText() );
-				} else {
-					$wgOut->showFatalError( $error );
-				}
+				$wgOut->showFatalError( $error );
 			}
 		}
 	}

includes/EditPage.php
@@ -1581,6 +1581,12 @@ END
 END
 );
 
+		if (!$this->preview) {
+			$wgOut->addHTML( Xml::tags( 'div',
+										array( 'class' => 'catlinks catlinks-allhidden',
+												'id' => 'catlinks' ), ' ' ) );
+		}
+
 		if ( $this->isConflict && wfRunHooks( 'EditPageBeforeConflictDiff', array( &$this, &$wgOut ) ) ) {
 			$wgOut->wrapWikiMsg( '==$1==', "yourdiff" );
 
@@ -2316,36 +2322,6 @@ END
 
 		wfRunHooks( 'EditPageBeforeEditButtons', array( &$this, &$buttons, &$tabindex ) );
 		return $buttons;
-	}
-
-	/**
-	 * Output preview text only. This can be sucked into the edit page
-	 * via JavaScript, and saves the server time rendering the skin as
-	 * well as theoretically being more robust on the client (doesn't
-	 * disturb the edit box's undo history, won't eat your text on
-	 * failure, etc).
-	 *
-	 * @todo This doesn't include category or interlanguage links.
-	 *       Would need to enhance it a bit, <s>maybe wrap them in XML
-	 *       or something...</s> that might also require more skin
-	 *       initialization, so check whether that's a problem.
-	 */
-	function livePreview() {
-		global $wgOut;
-		$wgOut->disable();
-		header( 'Content-type: text/xml; charset=utf-8' );
-		header( 'Cache-control: no-cache' );
-
-		$previewText = $this->getPreviewText();
-		#$categories = $skin->getCategoryLinks();
-
-		$s =
-		'<?xml version="1.0" encoding="UTF-8" ?>' . "\n" .
-		Xml::tags( 'livepreview', null,
-			Xml::element( 'preview', null, $previewText )
-			#.	Xml::element( 'category', null, $categories )
-		);
-		echo $s;
 	}
 
 

skins/common/preview.js
@@ -33,7 +33,10 @@ function doLivePreview( e ) {
 				//  with the content of the loaded page
 				var copyContent = page.find( copyElements[i] ).contents();
 				$j(copyElements[i]).empty().append( copyContent );
+				var newClasses = page.find( copyElements[i] ).attr('class');
+				$j(copyElements[i]).attr( 'class', newClasses );
 			}
+			
 			$j('#wikiPreview').slideDown();
 		} );
 }

commit 1c49152299c03ed236721bd6ff3677c574dd2176

Fix bug 20688 (Hidden categories still hidden if there are no non-hidden categories and the showhiddencats option is on). Regression in r55573.
svn path=/trunk/phase3/; revision=56523

includes/Skin.php
@@ -895,11 +895,17 @@ END;
 	}
 
 	function getCategories() {
-		$catlinks=$this->getCategoryLinks();
+		$catlinks = $this->getCategoryLinks();
 
 		$classes = 'catlinks';
+		
+		// Check what we're showing
+		global $wgOut, $wgUser;
+		$allCats = $wgOut->getCategoryLinks();
+		$showHidden = $wgUser->getBoolOption( 'showhiddencats' ) ||
+						$this->mTitle->getNamespace() == NS_CATEGORY;
 
-		if( strpos( $catlinks, '<div id="mw-normal-catlinks">' ) === false ) {
+		if( empty($allCats['normal']) && !( !empty($allCats['hidden']) && $showHidden ) ) {
 			$classes .= ' catlinks-allhidden';
 		}
 