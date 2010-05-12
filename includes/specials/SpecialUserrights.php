bymerej/mwphb

commit ab861c7cf4928ea378a5c5298ab160c80b9a963f

Fix regression from r54429 "(bug 17864 + bug 19519) - Do input normalization on the username before doing existence/permission checks"
This did some overactive canonicalization of the input username before username and database were split, breaking access to remote users where the database name contained an underscore -- it was changed to space.
This would also have broken numeric access, I think, but I didn't bother to test that. :)
Fix moves the username canonicalization down into fetchUser() after we split the name and database, so we now can access remote folks via non-canonical forms and we don't break the dbnames.

svn path=/trunk/phase3/; revision=57064

includes/specials/SpecialUserrights.php
@@ -54,7 +54,6 @@ class UserrightsPage extends SpecialPage {
 		} else {
 			$this->mTarget = $wgRequest->getVal( 'user' );
 		}
-		$this->mTarget = User::getCanonicalName( $this->mTarget );
 
 		/*
 		 * If the user is blocked and they only have "partial" access
@@ -311,6 +310,12 @@ class UserrightsPage extends SpecialPage {
 
 			if( !$name ) {
 				return new WikiErrorMsg( 'noname' );
+			}
+		} else {
+			$name = User::getCanonicalName( $name );
+			if( !$name ) {
+				// invalid name
+				return new WikiErrorMsg( 'nosuchusershort', $username );
 			}
 		}
 

commit 0cabde908dd4ed29374df2983e5e396e96011056

Special:Userrights didn't recognize user as self if person didn't capitalize the first letter of their name, and thus wouldn't honour $wgGroupsAddToSelf if user accidently forgot to capitalize their name.
Possibly a regression from r57064

svn path=/trunk/phase3/; revision=66258

RELEASE-NOTES
@@ -162,6 +162,7 @@ in a negative namespace (which is invalid).
   MediaWiki:Subject-preview when previewing summary
 * (bug 23426) The {{REVISIONMONTH}} variable is now zero-padded and added new
   variable {{REVISIONMONTH1}} when unpadded version is needed.
+* Special:Userrights didn't recognize user as changing his/her own rights if user did not capitalize first letter of username.
 
 === API changes in 1.17 ===
 * (bug 22738) Allow filtering by action type on query=logevent

includes/specials/SpecialUserrights.php
@@ -77,7 +77,7 @@ class UserrightsPage extends SpecialPage {
 				$this->mTarget = $wgUser->getName();
 		}
 
-		if ( $this->mTarget == $wgUser->getName() )
+		if ( User::getCanonicalName( $this->mTarget ) == $wgUser->getName() )
 			$this->isself = true;
 
 		if( !$this->userCanChangeRights( $wgUser, true ) ) {