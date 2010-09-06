bymerej/mwphb

commit 08c222602980c7eff7f0f9c3b72e81091c599025

initPage() is far too late to be setting a title, so we'll do it right after newFromKey() is called. Allow getSkin() to be passed a title for context, else fall back to OutputPage's title.
svn path=/trunk/phase3/; revision=49493

includes/User.php
@@ -2144,11 +2144,12 @@ class User {
 	}
 
 	/**
-	 * Get the current skin, loading it if required
-	 * @return \type{Skin} Current skin
+	 * Get the current skin, loading it if required, and setting a title
+	 * @param Title $t The title to use in the skin
+	 * @return Skin The current skin
 	 * @todo FIXME : need to check the old failback system [AV]
 	 */
-	function &getSkin() {
+	function &getSkin( $t = null ) {
 		global $wgRequest, $wgAllowUserSkin, $wgDefaultSkin;
 		if ( ! isset( $this->mSkin ) ) {
 			wfProfileIn( __METHOD__ );
@@ -2165,6 +2166,11 @@ class User {
 			$this->mSkin =& Skin::newFromKey( $userSkin );
 			wfProfileOut( __METHOD__ );
 		}
+		if ( !$t ) {
+			global $wgOut;
+			$t = $wgOut->getTitle();
+		}
+		$this->mSkin->setTitle( $t );
 		return $this->mSkin;
 	}
 

includes/Skin.php
@@ -176,8 +176,6 @@ class Skin extends Linker {
 
 		wfProfileIn( __METHOD__ );
 
-		$this->mTitle = $out->getTitle();
-
 		# Generally the order of the favicon and apple-touch-icon links
 		# should not matter, but Konqueror (3.5.9 at least) incorrectly
 		# uses whichever one appears later in the HTML source.  Make sure
@@ -268,11 +266,22 @@ class Skin extends Linker {
 		}
 	}
 
-	function setMembers(){
+	/**
+	 * Set some local globals
+	 */
+	protected function setMembers(){
 		global $wgUser;
 		$this->mUser = $wgUser;
 		$this->userpage = $wgUser->getUserPage()->getPrefixedText();
 		$this->usercss = false;
+	}
+	
+	/**
+	 * Set the title
+	 * @param Title $t The title to use
+	 */
+	public function setTitle( $t ) {
+		$this->mTitle = $t;
 	}
 
 	function outputPage( OutputPage $out ) {

commit 04555313418783c0eae615fa062d795f9566a0e4

Rewrite User::getSkin, broken in r49493 because requesting the skin for a particular title had the side-effect of changing the title associated with the stored Skin object, causing weirdness like the wrong namespace tabs.
svn path=/trunk/phase3/; revision=72481

includes/User.php
@@ -2216,33 +2216,46 @@ class User {
 	 * @return Skin The current skin
 	 * @todo FIXME : need to check the old failback system [AV]
 	 */
-	function &getSkin( $t = null ) {
-		if ( !isset( $this->mSkin ) ) {
-			wfProfileIn( __METHOD__ );
-
-			global $wgHiddenPrefs;
-			if( !in_array( 'skin', $wgHiddenPrefs ) ) {
-				# get the user skin
-				global $wgRequest;
-				$userSkin = $this->getOption( 'skin' );
-				$userSkin = $wgRequest->getVal( 'useskin', $userSkin );
-			} else {
-				# if we're not allowing users to override, then use the default
-				global $wgDefaultSkin;
-				$userSkin = $wgDefaultSkin;
+	function getSkin( $t = null ) {
+		if ( $t ) {
+			$skin = $this->createSkinObject();
+			$skin->setTitle( $t );
+			return $skin;
+		} else {
+			if ( ! $this->mSkin ) {
+				$this->mSkin = $this->createSkinObject();
 			}
-
-			$this->mSkin = Skin::newFromKey( $userSkin );
-			wfProfileOut( __METHOD__ );
-		}
-		if( $t || !$this->mSkin->getTitle() ) {
-			if ( !$t ) {
+			
+			if ( ! $this->mSkin->getTitle() ) {
 				global $wgOut;
 				$t = $wgOut->getTitle();
+				$this->mSkin->setTitle($t);
 			}
-			$this->mSkin->setTitle( $t );
+			
+			return $this->mSkin;
 		}
-		return $this->mSkin;
+	}
+	
+	// Creates a Skin object, for getSkin()
+	private function createSkinObject() {
+		wfProfileIn( __METHOD__ );
+
+		global $wgHiddenPrefs;
+		if( !in_array( 'skin', $wgHiddenPrefs ) ) {
+			# get the user skin
+			global $wgRequest;
+			$userSkin = $this->getOption( 'skin' );
+			$userSkin = $wgRequest->getVal( 'useskin', $userSkin );
+		} else {
+			# if we're not allowing users to override, then use the default
+			global $wgDefaultSkin;
+			$userSkin = $wgDefaultSkin;
+		}
+
+		$skin = Skin::newFromKey( $userSkin );
+		wfProfileOut( __METHOD__ );
+		
+		return $skin;
 	}
 
 	/**