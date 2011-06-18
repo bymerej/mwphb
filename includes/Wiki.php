bymerej/mwphb

commit 0a890bd925cc66eeb7874503e7fcc8bca892b264

Some HipHop fixes: * Scan the C++ for volatile classes and show a warning with a list of them * Fixed volatile classes Revision, CoreLinkFunctions and FileRepoStatus, made them non-volatile by patching the referring code * Added some configuration for the build process to DefaultSettings.php. * Split a few functions off MakeHipHop::execute() * Only include UtfNormalDefines.php in interpreted mode, since in compiled mode, the constants exist from startup * Apparently HipHop does not support set_exception_handler(). Added a try/catch block around the main part of index.php instead. * Fixed ini_get() dependencies in Special:Upload. Upload now works, if you disable ZipDirectoryReader.
svn path=/trunk/phase3/; revision=88959

index.php
@@ -66,7 +66,11 @@ ENDL;
 # does *not* load $wgTitle
 require ( dirname( __FILE__ ) . '/includes/WebStart.php' );
 
-wfIndexMain();
+try {
+	wfIndexMain();
+} catch ( Exception $e ) {
+	wfExceptionHandler( $e );
+}
 
 function wfIndexMain() {
 	global $wgRequest, $wgShowHostnames, $mediaWiki, $wgTitle, $wgUseAjax, $wgUseFileCache;

includes/DefaultSettings.php
@@ -5309,6 +5309,25 @@ $wgUpdateRowsPerQuery = 100;
 /** @} */ # End job queue }
 
 /************************************************************************//**
+ * @name   HipHop compilation
+ * @{
+ */
+
+/**
+ * The HipHop build type. Can be either "Debug" or "Release".
+ */
+$wgHipHopBuildType = 'Debug';
+
+/**
+ * Number of parallel processes to use during HipHop compilation, or "detect"
+ * to guess from system properties.
+ */
+$wgHipHopCompilerProcs = 'detect';
+
+/** @} */ # End of HipHop compilation }
+
+
+/************************************************************************//**
  * @name   Miscellaneous
  * @{
  */

includes/Init.php
@@ -90,4 +90,13 @@ class MWInit {
 		}
 		return $r !== false;
 	}
+
+	/**
+	 * Call a static method of a class with variable arguments without causing 
+	 * it to become volatile.
+	 */
+	static function callStaticMethod( $className, $methodName, $args ) {
+		$r = new ReflectionMethod( $className, $methodName );
+		return $r->invokeArgs( null, $args );
+	}
 }

includes/Setup.php
@@ -337,9 +337,9 @@ if ( !defined( 'MW_COMPILED' ) ) {
 	require_once( "$IP/includes/Hooks.php" );
 	require_once( "$IP/includes/ProxyTools.php" );
 	require_once( "$IP/includes/ImageFunctions.php" );
+	require_once( "$IP/includes/normal/UtfNormalDefines.php" );
 	wfProfileOut( $fname . '-includes' );
 }
-require_once( MWInit::compiledPath( 'includes/normal/UtfNormalDefines.php' ) );
 
 wfProfileIn( $fname . '-misc1' );
 

includes/filerepo/FileRepo.php
@@ -606,7 +606,7 @@ abstract class FileRepo {
 	function newFatal( $message /*, parameters...*/ ) {
 		$params = func_get_args();
 		array_unshift( $params, $this );
-		return call_user_func_array( array( 'FileRepoStatus', 'newFatal' ), $params );
+		return MWInit::callStaticMethod( 'FileRepoStatus', 'newFatal', $params );
 	}
 
 	/**

includes/parser/Parser_LinkHooks.php
@@ -254,8 +254,8 @@ class Parser_LinkHooks extends Parser {
 		}
 		if( $return === true ) {
 			# True (treat as plain link) was returned, call the defaultLinkHook
-			$args = array( $parser, $holders, $markers, $title, $titleText, &$paramText, &$leadingColon );
-			$return = call_user_func_array( array( 'CoreLinkFunctions', 'defaultLinkHook' ), $args );
+			$return = CoreLinkFunctions::defaultLinkHook( $parser, $holders, $markers, $title, 
+				$titleText, $paramText, $leadingColon );
 		}
 		if( $return === false ) {
 			# False (no link) was returned, output plain wikitext

includes/specials/SpecialUpload.php
@@ -863,9 +863,13 @@ class UploadForm extends HTMLForm {
 			);
 		}
 
-		$this->mMaxUploadSize['file'] = min(
-			wfShorthandToInteger( ini_get( 'upload_max_filesize' ) ),
-			UploadBase::getMaxUploadSize( 'file' ) );
+		$this->mMaxUploadSize['file'] = UploadBase::getMaxUploadSize( 'file' );
+		# Limit to upload_max_filesize unless we are running under HipHop and 
+		# that setting doesn't exist
+		if ( !wfIsHipHop() ) {
+			$this->mMaxUploadSize['file'] = min( $this->mMaxUploadSize['file'],
+				wfShorthandToInteger( ini_get( 'upload_max_filesize' ) ) );
+		}
 
 		$descriptor['UploadFile'] = array(
 			'class' => 'UploadSourceField',

includes/upload/UploadBase.php
@@ -75,7 +75,7 @@ abstract class UploadBase {
 		}
 
 		# Check php's file_uploads setting
-		if( !wfIniGetBool( 'file_uploads' ) ) {
+		if( !wfIsHipHop() && !wfIniGetBool( 'file_uploads' ) ) {
 			return false;
 		}
 		return true;

commit 0fbd39bd478345969f37ce2aa0912686765bb3cb

As discussed in r85918 CR. Move everything from wfIndexMain() at index.php to MediaWiki class. This makes index.php compatible again with PHP4 (as in showing a nice error message instead of a parser error), which had been broken in r88959.
svn path=/trunk/phase3/; revision=90338

includes/Wiki.php
@@ -22,7 +22,11 @@ class MediaWiki {
 		return $this->context->getOutput();
 	}
 
-	public function __construct( RequestContext $context ){
+	public function __construct( RequestContext $context = null ) {
+		if ( !$context ) {
+			$context = RequestContext::getMain();
+		}
+		
 		$this->context = $context;
 		$this->context->setTitle( $this->parseTitle() );
 	}
@@ -472,5 +476,101 @@ class MediaWiki {
 				}
 		}
 		wfProfileOut( __METHOD__ );
+	}
+
+	/**
+	 * Run the current MediaWiki instance
+	 * index.php just calls this 
+	 */	
+	function run() {
+		try {
+			$this->checkMaxLag( true );
+			$this->main();
+			$this->finalCleanup();
+			$this->restInPeace();
+		} catch ( Exception $e ) {
+			MWExceptionHandler::handle( $e );
+		}
+	} 
+	
+	/**
+	 * Checks if the request should abort due to a lagged server, 
+	 * for given maxlag parameter.
+	 * 
+	 * @param boolean $abort True if this class should abort the 
+	 * script execution. False to return the result as a boolean.
+	 * @return boolean True if we passed the check, false if we surpass the maxlag
+	 */
+	function checkMaxLag( $abort ) {
+		global $wgShowHostnames;
+		
+		wfProfileIn( __METHOD__ );
+		$maxLag = $this->context->getRequest()->getVal( 'maxlag' );
+		if ( !is_null( $maxLag ) ) {
+			$lb = wfGetLB(); // foo()->bar() is not supported in PHP4
+			list( $host, $lag ) = $lb->getMaxLag();
+			if ( $lag > $maxLag ) {
+				if ( $abort ) {
+					header( 'HTTP/1.1 503 Service Unavailable' );
+					header( 'Retry-After: ' . max( intval( $maxLag ), 5 ) );
+					header( 'X-Database-Lag: ' . intval( $lag ) );
+					header( 'Content-Type: text/plain' );
+					if( $wgShowHostnames ) {
+						echo "Waiting for $host: $lag seconds lagged\n";
+					} else {
+						echo "Waiting for a database server: $lag seconds lagged\n";
+					}
+				}
+				
+				wfProfileOut( __METHOD__ );
+
+				if ( !$abort ) 
+					return false;
+				exit;
+			}
+		}
+		wfProfileOut( __METHOD__ );
+		return true;
+	}
+
+	function main() {
+		global $wgUseFileCache, $wgTitle, $wgUseAjax;
+		
+		# Set title from request parameters
+		$wgTitle = $this->getTitle();
+		$action = $this->context->getRequest()->getVal( 'action', 'view' );
+		
+		# Send Ajax requests to the Ajax dispatcher.
+		if ( $wgUseAjax && $action == 'ajax' ) {
+			$dispatcher = new AjaxDispatcher();
+			$dispatcher->performAction();
+			return;
+		}
+		
+		if ( $wgUseFileCache && $wgTitle !== null ) {
+			wfProfileIn( 'main-try-filecache' );
+			// Raw pages should handle cache control on their own,
+			// even when using file cache. This reduces hits from clients.
+			if ( $action != 'raw' && HTMLFileCache::useFileCache() ) {
+				/* Try low-level file cache hit */
+				$cache = new HTMLFileCache( $wgTitle, $action );
+				if ( $cache->isFileCacheGood( /* Assume up to date */ ) ) {
+					/* Check incoming headers to see if client has this cached */
+					if ( !$this->context->getOutput()->checkLastModified( $cache->fileCacheTime() ) ) {
+						$cache->loadFromFileCache();
+					}
+					# Do any stats increment/watchlist stuff
+					$article = Article::newFromTitle( $wgTitle, $this->context );
+					$article->viewUpdates();
+					# Tell OutputPage that output is taken care of
+					$this->context->getOutput()->disable();
+					wfProfileOut( 'main-try-filecache' );
+					return;
+				}
+			}
+			wfProfileOut( 'main-try-filecache' );
+		}
+		
+		$this->performRequest();
 	}
 }

index.php
@@ -70,99 +70,8 @@ if ( isset( $_SERVER['MW_COMPILED'] ) ) {
 	require ( dirname( __FILE__ ) . '/includes/WebStart.php' );
 }
 
-try {
-	wfIndexMain();
-} catch ( Exception $e ) {
-	MWExceptionHandler::handle( $e );
-}
-
-function wfIndexMain() {
-	global $wgRequest, $wgShowHostnames, $mediaWiki, $wgTitle, $wgUseAjax, $wgUseFileCache;
-
-	wfProfileIn( 'index.php' );
-	wfProfileIn( 'index.php-setup' );
-
-	$maxLag = $wgRequest->getVal( 'maxlag' );
-	if ( !is_null( $maxLag ) ) {
-		$lb = wfGetLB(); // foo()->bar() is not supported in PHP4
-		list( $host, $lag ) = $lb->getMaxLag();
-		if ( $lag > $maxLag ) {
-			header( 'HTTP/1.1 503 Service Unavailable' );
-			header( 'Retry-After: ' . max( intval( $maxLag ), 5 ) );
-			header( 'X-Database-Lag: ' . intval( $lag ) );
-			header( 'Content-Type: text/plain' );
-			if( $wgShowHostnames ) {
-				echo "Waiting for $host: $lag seconds lagged\n";
-			} else {
-				echo "Waiting for a database server: $lag seconds lagged\n";
-			}
-			wfProfileOut( 'index.php-setup' );
-			wfProfileOut( 'index.php' );
-			exit;
-		}
-	}
-
-	# Initialize MediaWiki base class
-	$context = RequestContext::getMain();
-	$mediaWiki = new MediaWiki( $context );
-
-	# Set title from request parameters
-	$wgTitle = $mediaWiki->getTitle();
-	$action = $wgRequest->getVal( 'action', 'view' );
-
-	wfProfileOut( 'index.php-setup' );
-
-	# Send Ajax requests to the Ajax dispatcher.
-	if ( $wgUseAjax && $action == 'ajax' ) {
-		$dispatcher = new AjaxDispatcher();
-		$dispatcher->performAction();
-		wfProfileOut( 'index.php' );
-		$mediaWiki->restInPeace();
-		exit;
-	}
-
-	if ( $wgUseFileCache && $wgTitle !== null ) {
-		wfProfileIn( 'index.php-filecache' );
-		// Raw pages should handle cache control on their own,
-		// even when using file cache. This reduces hits from clients.
-		if ( $action != 'raw' && HTMLFileCache::useFileCache() ) {
-			/* Try low-level file cache hit */
-			$cache = new HTMLFileCache( $wgTitle, $action );
-			if ( $cache->isFileCacheGood( /* Assume up to date */ ) ) {
-				/* Check incoming headers to see if client has this cached */
-				if ( !$context->getOutput()->checkLastModified( $cache->fileCacheTime() ) ) {
-					$cache->loadFromFileCache();
-				}
-				# Do any stats increment/watchlist stuff
-				$article = Article::newFromTitle( $wgTitle, $context );
-				$article->viewUpdates();
-				# Tell OutputPage that output is taken care of
-				$context->getOutput()->disable();
-				wfProfileOut( 'index.php-filecache' );
-				$mediaWiki->finalCleanup();
-				wfProfileOut( 'index.php' );
-				$mediaWiki->restInPeace();
-				exit;
-			}
-		}
-		wfProfileOut( 'index.php-filecache' );
-	}
-
-	/**
-	 * $wgArticle is deprecated, do not use it. This will possibly be removed
-	 * entirely in 1.20 or 1.21
-	 * @deprecated since 1.19
-	 */
-	global $wgArticle;
-
-	$wgArticle = $mediaWiki->performRequest();
-
-	$mediaWiki->finalCleanup();
-
-	wfProfileOut( 'index.php' );
-
-	$mediaWiki->restInPeace();
-}
+$mediaWiki = new MediaWiki();
+$mediaWiki->run();
 
 /**
  * Display something vaguely comprehensible in the event of a totally unrecoverable error.