bymerej/mwphb

commit 0224eb305a0d1b0d4d5fa2c5b55ea843ecd7b9b4

Rewrote Special:Upload to allow easier extension. Mostly backwards compatible towards the end user: tested with Commons' upload scripts. * Special:Upload now uses HTMLForm for form generation * Upload errors that can be solved by changing the filename now do not require reuploading.
svn path=/trunk/phase3/; revision=57868

languages/messages/MessagesEn.php
@@ -2038,8 +2038,7 @@ Pages on [[Special:Watchlist|your watchlist]] are '''bold'''.",
 # Upload
 'upload'                      => 'Upload file',
 'uploadbtn'                   => 'Upload file',
-'reupload'                    => 'Re-upload',
-'reuploaddesc'                => 'Cancel upload and return to the upload form',
+'reuploaddesc'                => 'Cancel upload and return to the upload form ',
 'uploadnologin'               => 'Not logged in',
 'uploadnologintext'           => 'You must be [[Special:UserLogin|logged in]] to upload files.',
 'upload_directory_missing'    => 'The upload directory ($1) is missing and could not be created by the webserver.',
@@ -2126,9 +2125,13 @@ Please check the file_uploads setting.',
 'uploadcorrupt'               => 'The file is corrupt or has an incorrect extension.
 Please check the file and upload again.',
 'uploadvirus'                 => 'The file contains a virus! Details: $1',
+'upload-source'               => 'Source file',
 'sourcefilename'              => 'Source filename:',
+'sourceurl'                   => 'Source URL:',
 'destfilename'                => 'Destination filename:',
 'upload-maxfilesize'          => 'Maximum file size: $1',
+'upload-description'          => 'File description',
+'upload-options'              => '',
 'watchthisupload'             => 'Watch this file',
 'filewasdeleted'              => 'A file of this name has been previously uploaded and subsequently deleted.
 You should check the $1 before proceeding to upload it again.',

RELEASE-NOTES
@@ -249,6 +249,10 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
   limit exceeded, __NOINDEX__ tracking, etc) can now be disabled by setting the 
   system message ([[MediaWiki:expensive-parserfunction-category]] etc) to "-".
 * Added maintenance script sqlite.php for SQLite-specific maintenance tasks.
+* Rewrote Special:Upload to allow easier extension. 
+* Upload errors that can be solved by changing the filename now do not require
+  reuploading.
+
 
 === Bug fixes in 1.16 ===
 

includes/AutoLoader.php
@@ -550,6 +550,7 @@ $wgAutoloadLocalClasses = array(
 	'SpecialSearch' => 'includes/specials/SpecialSearch.php',
 	'SpecialStatistics' => 'includes/specials/SpecialStatistics.php',
 	'SpecialTags' => 'includes/specials/SpecialTags.php',
+	'SpecialUpload' => 'includes/specials/SpecialUpload.php',
 	'SpecialVersion' => 'includes/specials/SpecialVersion.php',
 	'SpecialWhatlinkshere' => 'includes/specials/SpecialWhatlinkshere.php',
 	'UncategorizedCategoriesPage' => 'includes/specials/SpecialUncategorizedcategories.php',

includes/Licenses.php
@@ -9,46 +9,39 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-class Licenses {
-	/**#@+
-	 * @private
-	 */
+class Licenses extends HTMLFormField {
 	/**
 	 * @var string
 	 */
-	var $msg;
+	protected $msg;
 
 	/**
 	 * @var array
 	 */
-	var $licenses = array();
+	protected $licenses = array();
 
 	/**
 	 * @var string
 	 */
-	var $html;
+	protected $html;
 	/**#@-*/
 
 	/**
 	 * Constructor
-	 *
-	 * @param $str String: the string to build the licenses member from, will use
-	 *                    wfMsgForContent( 'licenses' ) if null (default: null)
 	 */
-	function __construct( $str = null ) {
-		// PHP sucks, this should be possible in the constructor
-		$this->msg = is_null( $str ) ? wfMsgForContent( 'licenses' ) : $str;
-		$this->html = '';
+	public function __construct( $params ) {
+		parent::__construct( $params );
+		
+		$this->msg = empty( $params['licenses'] ) ? wfMsgForContent( 'licenses' ) : $params['licenses'];
+		$this->selected = null;
 
 		$this->makeLicenses();
-		$tmp = $this->getLicenses();
-		$this->makeHtml( $tmp );
 	}
 
 	/**#@+
 	 * @private
 	 */
-	function makeLicenses() {
+	protected function makeLicenses() {
 		$levels = array();
 		$lines = explode( "\n", $this->msg );
 
@@ -75,18 +68,17 @@ class Licenses {
 		}
 	}
 
-	function trimStars( $str ) {
+	protected static function trimStars( $str ) {
 		$i = $count = 0;
 
-		wfSuppressWarnings();
-		while ($str[$i++] == '*')
-			++$count;
-		wfRestoreWarnings();
-
-		return array( $count, ltrim( $str, '* ' ) );
+		$length = strlen( $str );
+		for ( $i = 0; $i < $length; $i++ ) {
+			if ( $str[$i] != '*' )
+				return array( $i, ltrim( $str, '* ' ) );
+		}
 	}
 
-	function stackItem( &$list, $path, $item ) {
+	protected function stackItem( &$list, $path, $item ) {
 		$position =& $list;
 		if ( $path )
 			foreach( $path as $key )
@@ -94,13 +86,12 @@ class Licenses {
 		$position[] = $item;
 	}
 
-	function makeHtml( &$tagset, $depth = 0 ) {
+	protected function makeHtml( &$tagset, $depth = 0 ) {
 		foreach ( $tagset as $key => $val )
 			if ( is_array( $val ) ) {
 				$this->html .= $this->outputOption(
-					$this->msg( $key ),
+					$this->msg( $key ), '',
 					array(
-						'value' => '',
 						'disabled' => 'disabled',
 						'style' => 'color: GrayText', // for MSIE
 					),
@@ -109,22 +100,22 @@ class Licenses {
 				$this->makeHtml( $val, $depth + 1 );
 			} else {
 				$this->html .= $this->outputOption(
-					$this->msg( $val->text ),
-					array(
-						'value' => $val->template,
-						'title' => '{{' . $val->template . '}}'
-					),
+					$this->msg( $val->text ), $val->template,
+					array( 'title' => '{{' . $val->template . '}}' ),
 					$depth
 				);
 			}
 	}
 
-	function outputOption( $val, $attribs = null, $depth ) {
-		$val = str_repeat( /* &nbsp */ "\xc2\xa0", $depth * 2 ) . $val;
+	protected function outputOption( $text, $value, $attribs = null, $depth = 0 ) {
+		$attribs['value'] = $value;
+		if ( $value === $this->selected )
+			$attribs['selected'] = 'selected';		
+		$val = str_repeat( /* &nbsp */ "\xc2\xa0", $depth * 2 ) . $text;
 		return str_repeat( "\t", $depth ) . Xml::element( 'option', $attribs, $val ) . "\n";
 	}
 
-	function msg( $str ) {
+	protected function msg( $str ) {
 		$out = wfMsg( $str );
 		return wfEmptyMsg( $str, $out ) ? $str : $out;
 	}
@@ -136,14 +127,29 @@ class Licenses {
 	 *
 	 * @return array
 	 */
-	function getLicenses() { return $this->licenses; }
+	public function getLicenses() { return $this->licenses; }
 
 	/**
 	 * Accessor for $this->html
 	 *
 	 * @return string
 	 */
-	function getHtml() { return $this->html; }
+	public function getInputHTML( $value ) {
+		$this->selected = $value;
+		
+		$this->html = $this->outputOption( wfMsg( 'nolicense' ), '',
+			(bool)$this->selected ? null : array( 'selected' => 'selected' ) );
+		$this->makeHtml( $this->getLicenses() );
+		
+		$attribs = array(
+			'name' => $this->mName,
+			'id' => $this->mID
+		);
+		if ( !empty( $this->mParams['disabled'] ) )
+			$attibs['disabled'] = 'disabled';
+		
+		return Html::rawElement( 'select', $attribs, $this->html );
+	}
 }
 
 /**

includes/Setup.php
@@ -306,9 +306,9 @@ $wgDeferredUpdateList = array();
 $wgPostCommitUpdateList = array();
 
 if ( $wgAjaxWatch ) $wgAjaxExportList[] = 'wfAjaxWatch';
-if ( $wgAjaxUploadDestCheck ) $wgAjaxExportList[] = 'UploadForm::ajaxGetExistsWarning';
+if ( $wgAjaxUploadDestCheck ) $wgAjaxExportList[] = 'SpecialUpload::ajaxGetExistsWarning';
 if( $wgAjaxLicensePreview )
-	$wgAjaxExportList[] = 'UploadForm::ajaxGetLicensePreview';
+	$wgAjaxExportList[] = 'SpecialUpload::ajaxGetLicensePreview';
 
 # Placeholders in case of DB error
 $wgTitle = null;

includes/SpecialPage.php
@@ -141,7 +141,7 @@ class SpecialPage {
 		'Filepath'                  => array( 'SpecialPage', 'Filepath' ),
 		'MIMEsearch'                => array( 'SpecialPage', 'MIMEsearch' ),
 		'FileDuplicateSearch'       => array( 'SpecialPage', 'FileDuplicateSearch' ),
-		'Upload'                    => 'UploadForm',
+		'Upload'                    => 'SpecialUpload',
 
 		# Wiki data and tools
 		'Statistics'				=> 'SpecialStatistics',

includes/api/ApiUpload.php
@@ -230,7 +230,7 @@ class ApiUpload extends ApiBase {
 					$this->dieUsage( 'This file did not pass file verification', 'verification-error',
 							0, array( 'details' => $verification['details'] ) );
 					break;
-				case UploadBase::UPLOAD_VERIFICATION_ERROR:
+				case UploadBase::HOOK_ABORTED:
 					$this->dieUsage( "The modification you tried to make was aborted by an extension hook",
 							'hookaborted', 0, array( 'error' => $verification['error'] ) );
 					break;

commit 0550eff07657b14c8ae164da8083c9c1ebfec6f3

Remove 'upload-wasdeleted' message. Its no longer used.
Original feature was (accidently?) removed/broken in r57868 and then re-created
using the message 'upload-recreate-warning' in r65339.

svn path=/trunk/phase3/; revision=91594

languages/messages/MessagesEn.php
@@ -2187,10 +2187,6 @@ Uploading Java files is not allowed, because they can cause security restriction
 'watchthisupload'             => 'Watch this file',
 'filewasdeleted'              => 'A file of this name has been previously uploaded and subsequently deleted.
 You should check the $1 before proceeding to upload it again.',
-'upload-wasdeleted'           => "'''Warning: You are uploading a file that was previously deleted.'''
-
-You should consider whether it is appropriate to continue uploading this file.
-The deletion log for this file is provided here for convenience:",
 'filename-bad-prefix'         => "The name of the file you are uploading begins with '''\"\$1\"''', which is a non-descriptive name typically assigned automatically by digital cameras.
 Please choose a more descriptive name for your file.",
 'filename-prefix-blacklist'   => ' #<!-- leave this line exactly as it is --> <pre>

maintenance/language/messages.inc
@@ -1318,7 +1318,6 @@ $wgMessageStructure = array(
 		'upload-options',
 		'watchthisupload',
 		'filewasdeleted',
-		'upload-wasdeleted',
 		'filename-bad-prefix',
 		'filename-prefix-blacklist',
 		'upload-success-subj',